4c4ce42739a1c609be0bfb59ef043ee12cb1e3ca
Jefferson de Souza Dias <soudias09@gmail.com>
2025-05-02 16:07:13 +0000

    Completei o desafio aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
+//Variáveis com número 1 relacionadas à carta 1. Com número 2 relacionadas à carta 2.
 
-
-int main() {
+int main(){
+    
     char estado1, estado2; //Estado
     char nome_cidade1[15], nome_cidade2[15]; //Nome da cidade
     char cod_carta1[10], cod_carta2[10]; //Código da Carta
@@ -10,6 +11,8 @@ int main() {
     int pontos_turisticos1, pontos_turisticos2; //Pontos Turísticos
     float area1, area2; //Área em Km²
     float pib1, pib2; //PIB
+    float pib_cap1, pib_cap2;
+    float desin_populacao1, desin_populacao2;
 
     printf("SUPER TRUNFO\n");
 
@@ -61,13 +64,19 @@ int main() {
     printf("Quantos pontos turísticos tem a cidade: ");
     scanf("%d", &pontos_turisticos2);
 
+    pib_cap1 = pib1 / populacao1;
+    desin_populacao1 = populacao1 / area1;
+
+    pib_cap2 = pib2 / populacao2;
+    desin_populacao2 = populacao2 / area2;
+
     printf("\n");
 
-    printf("CARTA 1\n Estado: %c \n Código: %s \n Nome da Cidade: %s \n População: %d \n Área: %.3f km² \n PIB: %.1f Bilhões de reais \n Número de Pontos Turísticos: %d \n", estado1, cod_carta1, nome_cidade1, populacao1, area1, pib1, pontos_turisticos1);
+    printf("CARTA 1\n Estado: %c \n Código: %s \n Nome da Cidade: %s \n População: %d \n Área: %.2f km² \n PIB: %.2f Bilhões de reais \n Número de Pontos Turísticos: %d \n Densidade Populacional: %.2f \n PIB per Capta: %f \n", estado1, cod_carta1, nome_cidade1, populacao1, area1, pib1, pontos_turisticos1, desin_populacao1, pib_cap1);
 
     printf("\n");
 
-    printf("CARTA 2\n Estado: %c \n Código: %s \n Nome da Cidade: %s \n População: %d \n Área: %.3f km² \n PIB: %.1f Bilhões de reais \n Número de Pontos Turísticos: %d \n", estado2, cod_carta2, nome_cidade2, populacao2, area2, pib2, pontos_turisticos2);
+    printf("CARTA 2\n Estado: %c \n Código: %s \n Nome da Cidade: %s \n População: %d \n Área: %.2f km² \n PIB: %.2f Bilhões de reais \n Número de Pontos Turísticos: %d \n Densidade Populacional: %.2f \n PIB per Capta: %f \n", estado2, cod_carta2, nome_cidade2, populacao2, area2, pib2, pontos_turisticos2, desin_populacao2, pib_cap2);
 
     return 0;
 }
